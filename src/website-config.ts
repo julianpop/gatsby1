a24de2de1ea104908ee196d8ddea3ce2fe87eed0
Julian <julianpop@hotmail.com>
2019-01-26 20:01:35 -0600

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -29,14 +29,14 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Gatsby One',
+  description: 'My first Gatsby project',
   coverImage: 'img/blog-cover.jpg',
-  logo: 'img/ghost-logo.png',
+  logo: 'https://xcedo.com/img/avatar.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://thirsty-wilson-b1bf05.netlify.com/',
+  /* facebook: 'https://www.facebook.com/ghost', */
+  /* twitter: 'https://twitter.com/tryghost', */
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
